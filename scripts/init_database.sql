15f0fc575cca67f0ab61cbfae408b3ecd489c782
yashmamchad-jpg <yashmamchad@gmail.com>
2025-10-02 02:06:04 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,10 @@
+-- Create Database and Schemas
+-- Script Purpose: This script creates a new database called "DATAWAREHOUSE".
+-- The script sets-up three schemas Bronze, Silver, Gold.
+
+
+create database DATAWAREHOUSE;
+use DATAWAREHOUSE;
+create schema Bronze;
+create schema Silver;
+create schema Gold;
