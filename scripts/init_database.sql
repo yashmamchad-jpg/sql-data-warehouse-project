9bd3cdfe5c4717f4466f7eaf3e8d43bdf372959c
yashmamchad-jpg <yashmamchad@gmail.com>
2025-10-02 02:43:29 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -15,3 +15,14 @@ CREATE SCHEMA Silver;
 GO
 CREATE SCHEMA Gold;
 GO
+  
+CREATE TABLE bronze.crm_cust_info(
+    cst_id INT,
+    cst_key NVARCHAR(50),
+    cst_firstname NVARCHAR(50),
+    cst_lastname NVARCHAR(50),
+    cst_material_status NVARCHAR(50),
+    cst_gndr NVARCHAR(50),
+    cst_create_date NVARCHAR(50),
+
+);
