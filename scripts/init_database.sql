64d59a8839d12e6c25e465a89bfd400c3a94a831
yashmamchad-jpg <yashmamchad@gmail.com>
2025-10-02 03:28:03 +0530

    init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -23,6 +23,48 @@ CREATE TABLE bronze.crm_cust_info(
     cst_lastname NVARCHAR(50),
     cst_material_status NVARCHAR(50),
     cst_gndr NVARCHAR(50),
-    cst_create_date NVARCHAR(50),
+    cst_create_date NVARCHAR(50)
+);
 
+
+CREATE TABLE bronze.crm_prd_info(
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost NVARCHAR(50),
+    prd_line NVARCHAR(50),
+    prd_start_dt NVARCHAR(50),
+    prd_end_dt NVARCHAR(50)
+);
+
+
+
+CREATE TABLE bronze.crm_sales_details(
+    sls_ord_num INT,
+    sls_prd_key NVARCHAR(50),
+    sls_cust_id NVARCHAR(50),
+    sls_order_dt NVARCHAR(50),
+    sls_ship_dt NVARCHAR(50),
+    sls_due_dt NVARCHAR(50),
+    sls_sales NVARCHAR(50),
+    sls_quantity NVARCHAR(50),
+    sls_price NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_cust_AZ12(
+    CID INT,
+    BDATE NVARCHAR(50),
+    GEN NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_loc_A101(
+    CID INT,
+    CNTRY NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_px_cat_G1V2(
+    ID INT,
+    CAT NVARCHAR(50),
+    SUBCAT NVARCHAR(50),
+    MAINTENANCE NVARCHAR(50),
 );
