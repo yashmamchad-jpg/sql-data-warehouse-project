e45120b667da548eac3b018ae0b87a8dafc2061e
yashmamchad-jpg <yashmamchad@gmail.com>
2025-10-02 03:36:41 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -31,39 +31,39 @@ CREATE TABLE bronze.crm_prd_info(
     prd_id INT,
     prd_key NVARCHAR(50),
     prd_nm NVARCHAR(50),
-    prd_cost NVARCHAR(50),
+    prd_cost INT,
     prd_line NVARCHAR(50),
-    prd_start_dt NVARCHAR(50),
-    prd_end_dt NVARCHAR(50)
+    prd_start_dt DATETIME,
+    prd_end_dt DATETIME
 );
 
 
 
 CREATE TABLE bronze.crm_sales_details(
-    sls_ord_num INT,
+    sls_ord_num NVARCHAR(50),
     sls_prd_key NVARCHAR(50),
-    sls_cust_id NVARCHAR(50),
-    sls_order_dt NVARCHAR(50),
-    sls_ship_dt NVARCHAR(50),
-    sls_due_dt NVARCHAR(50),
-    sls_sales NVARCHAR(50),
-    sls_quantity NVARCHAR(50),
-    sls_price NVARCHAR(50)
+    sls_cust_id INT,
+    sls_order_dt INT,
+    sls_ship_dt INT,
+    sls_due_dt INT,
+    sls_sales INT,
+    sls_quantity INT,
+    sls_price INT
 );
 
 CREATE TABLE bronze.erp_cust_AZ12(
-    CID INT,
-    BDATE NVARCHAR(50),
+    CID NVARCHAR(50),
+    BDATE DATE,
     GEN NVARCHAR(50)
 );
 
 CREATE TABLE bronze.erp_loc_A101(
-    CID INT,
+    CID NVARCHAR(50),
     CNTRY NVARCHAR(50)
 );
 
 CREATE TABLE bronze.erp_px_cat_G1V2(
-    ID INT,
+    ID NVARCHAR(50),
     CAT NVARCHAR(50),
     SUBCAT NVARCHAR(50),
     MAINTENANCE NVARCHAR(50),
